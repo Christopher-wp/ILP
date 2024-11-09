e1f9d84f68da6d8acd28ae3904ffd73e1127745b
Christopher-wp <valsmurffxd@gmail.com>
2024-11-09 12:43:52 -0600

    Add files via upload

---
+++ Operacionesmatematicas.py
@@ -0,0 +1,27 @@
+#operaciones matematicas 
+
+#DECLARAR O CREAR VARIABLES O CONSTANTES 
+nombre_completo = "Christopher Chong "
+edad = 33
+estatura = 1.82
+direccion = "calle x , colonia y"
+telefono = "55 11545121"
+correo = "tote@mx.com"
+activo = True
+#CONSTANTE
+PI = 3.1416
+GRAVEDAD = 9.81
+
+
+
+
+
+dato1 = 10
+dato2 = 12
+
+suma= dato1 + dato2 
+print("el resultado de la esuma es=",suma)
+resta = dato1 - dato2 
+print(resta)
+multiplicacion = dato1 * dato2 
+print (multiplicacion)
